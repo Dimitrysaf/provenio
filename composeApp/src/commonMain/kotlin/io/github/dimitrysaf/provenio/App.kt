2430cf8e355fc2b4f44df712e8fd4e92675addc9
Dimitrysaf <demetresmeliates+github@gmail.com>
2026-09-07 19:16:05 +0000

    fix: opt into experimental material3 app bar api

--- composeApp/src/commonMain/kotlin/io/github/dimitrysaf/provenio/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/dimitrysaf/provenio/App.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     MaterialTheme {
